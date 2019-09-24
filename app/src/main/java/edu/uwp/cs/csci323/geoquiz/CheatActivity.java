57dc32cab25315fb019f64fce2ed3c3df1e64917
Jeremy Wolfe <Wolfe032@rangers.uwp.edu>
2019-09-24 14:35:33 -0500

    Commit for Class
    Just testing this for class

---
+++ app/src/main/java/edu/uwp/cs/csci323/geoquiz/CheatActivity.java
@@ -0,0 +1,14 @@
+package edu.uwp.cs.csci323.geoquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CheatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cheat);
+    }
+}
